f3aa4ee48f4ee05a45b4d318a4533517321c3f74
Oleg <creduntvitam@gmail.com>
2024-04-08 16:11:40 +0200

    Added New wifi_monitoring function

--- main.go
+++ main.go
@@ -1,43 +1,9 @@
 package main
 
-import (
-	"fmt"
-	"net"
-	"os/exec"
-	"strings"
-)
+import "whoisonmywifi/wifi_monitoring"
 
 func main() {
-	// Get the local IP address
-	addrs, err := net.InterfaceAddrs()
-	if err != nil {
-		fmt.Println("Error:", err)
-		return
-	}
 
-	var localIP string
-	for _, addr := range addrs {
-		if ipnet, ok := addr.(*net.IPNet); ok && !ipnet.IP.IsLoopback() && ipnet.IP.To4() != nil {
-			localIP = ipnet.IP.String()
-			break
-		}
-	}
+	wifi_monitoring.MonitorWiFiConnections()
 
-	fmt.Println(localIP)
-	// Run ARP command to get the list of devices
-	cmd := exec.Command("arp", "-a")
-	output, err := cmd.Output()
-	if err != nil {
-		fmt.Println("Error:", err)
-		return
-	}
-
-	// Parse the ARP table and print the list of devices
-	lines := strings.Split(string(output), "\n")
-	for _, line := range lines {
-		fields := strings.Fields(line)
-		if len(fields) >= 3 {
-			fmt.Println(fields[0] + " : " + fields[1] + " : " + fields[2])
-		}
-	}
 }
--- wifi_monitoring/wifi_monitoring.go
+++ wifi_monitoring/wifi_monitoring.go
@@ -1,9 +1,39 @@
 // wifi_monitor/wifi_monitor.go
 
-package wifi_monitor
+package wifi_monitoring
+
+import (
+	"fmt"
+	"os/exec"
+	"strings"
+)
 
 // import necessary packages
 
-func MonitorWiFiConnections() {
-	// Logic to monitor Wi-Fi connections
+type WiFiConnection struct {
+	Internet string
+	Address  string
+	Physical string
+}
+
+func MonitorWiFiConnections() error {
+	// Execute the system command to retrieve the ARP table
+	cmd := exec.Command("arp", "-a")
+	output, err := cmd.Output()
+	if err != nil {
+		return err
+	}
+
+	// Parse the output to extract MAC addresses and IP addresses
+	lines := strings.Split(string(output), "\n")
+	for _, line := range lines {
+		fields := strings.Fields(line)
+		if len(fields) >= 3 && !strings.HasPrefix(fields[0], "Interface") {
+			ip := fields[0]
+			mac := fields[1]
+			fmt.Printf("IP: %s, MAC: %s\n", ip, mac)
+		}
+	}
+
+	return nil
 }
