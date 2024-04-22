ab75ee83f43c1d8ca58eff41043ddbca16a9c2bd
Oleg <creduntvitam@gmail.com>
2024-04-22 23:00:45 +0200

    Added Terminal command

--- main.go
+++ main.go
@@ -1,26 +1,49 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
 	device_details "whoisonmywifi/manage_device_details"
 	"whoisonmywifi/wifi_monitoring"
 )
 
 func main() {
 
-	// Function to to get WiFi Connection data
-	err, w := wifi_monitoring.MonitorWiFiConnections()
+	command := flag.NewFlagSet("getpcapinfo", flag.ExitOnError)
+	// Define flags for other commands if needed...
 
-	if err != nil {
-		return
+	// Check if there are any command-line arguments
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: whois <command>")
+		os.Exit(1)
 	}
 
-	for _, t := range *w {
-		fmt.Printf("IP: %s, MAC: %s, Type: %s\n", t.IP, t.Mac, t.Type)
-		// Get Device name from Router
-		result, _ := device_details.GetDeviceNameByMAC(t.Mac)
+	// Parse the command-line arguments
+	switch os.Args[1] {
+	case "getpcapinfo":
+		command.Parse(os.Args[2:])
 		device_details.GetDeviceInformationPCAP()
-		fmt.Print(result)
+		// Add cases for other commands if needed...
+	case "checkwifi":
+		command.Parse(os.Args[2:])
+		// Function to to get WiFi Connection data
+		err, w := wifi_monitoring.MonitorWiFiConnections()
+
+		if err != nil {
+			return
+		}
+
+		for _, t := range *w {
+			fmt.Printf("IP: %s, MAC: %s, Type: %s\n", t.IP, t.Mac, t.Type)
+			// Get Device name from Router
+			result, _ := device_details.GetDeviceNameByMAC(t.Mac)
+
+			fmt.Print(result)
+		}
+	default:
+		fmt.Println("Unknown command")
+		os.Exit(1)
 	}
 
 }
